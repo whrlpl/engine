58c99b82ab77d61f21cb342cf0b82e0a3909a8fb
Alex <alexnoir03@gmail.com>
2018-07-14 13:39:11 +0100

    [TYPE] Update Any to be compatible with all currently supported Material variable types.

--- Whirlpool.Core/Type/Any.cs
+++ Whirlpool.Core/Type/Any.cs
@@ -1,9 +1,6 @@
 ﻿using OpenTK;
+using OpenTK.Graphics;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Whirlpool.Core.Type
 {
@@ -20,11 +17,12 @@ namespace Whirlpool.Core.Type
 
         public dynamic GetValue() => Convert.ChangeType(value, valueType);
 
-        public static implicit operator Any(Matrix4 value) => new Any(value.GetType(), value);
+        public static implicit operator Any(bool value) => new Any(value.GetType(), value);
         public static implicit operator Any(float value) => new Any(value.GetType(), value);
         public static implicit operator Any(int value) => new Any(value.GetType(), value);
         public static implicit operator Any(Vector2 value) => new Any(value.GetType(), value);
-        public static implicit operator Any(bool value) => new Any(value.GetType(), value);
-        public static implicit operator Any(string value) => new Any(value.GetType(), value);
+        public static implicit operator Any(Vector3 value) => new Any(value.GetType(), value);
+        public static implicit operator Any(Color4 value) => new Any(value.GetType(), value);
+        public static implicit operator Any(Matrix4 value) => new Any(value.GetType(), value);
     }
 }
